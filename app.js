48583cf64d9d7055b181d0e4cdfd26aac4e4cded
agung <agunkootsuki123@gmail.com>
2023-12-04 06:48:02 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+let slideIndex = 0;
+showslide();
+
+function showslide() {
+  let i;
+  let slides = document.getElementsByClassName('slide');
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  setTimeout(showslide, 5000);
+}
